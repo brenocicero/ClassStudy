d0a0cb72b9a21fb86a6b7918f583042eeeba462b
brenocicero <brenocic@gmail.com>
2024-06-18 22:33:03 -0300

    test commit

---
+++ index.js
@@ -0,0 +1,19 @@
+class characters {
+    constructor(name, age, category){
+        this.name = name
+        this.age = age
+        this.category = category
+    }
+
+    atacar(){
+        if characters.category === mago{
+            console.log("usou magia")
+        }
+        if characters.category === guerreiro{
+
+        }
+
+    }
+}
+
+let batman = new characters("Batman", "45", "guerreiro")
